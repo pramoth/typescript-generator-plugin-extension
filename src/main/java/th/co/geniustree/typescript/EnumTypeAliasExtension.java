f497879464a3e6731e8baa55306cfba8f4e34fce
Pramoth <pramoth@geniustree.co.th>
2020-11-12 16:32:52 +0700

    setup

---
+++ src/main/java/th/co/geniustree/typescript/EnumTypeAliasExtension.java
@@ -0,0 +1,22 @@
+package th.co.geniustree.typescript;
+
+import cz.habarta.typescript.generator.Settings;
+import cz.habarta.typescript.generator.emitter.EmitterExtension;
+import cz.habarta.typescript.generator.emitter.EmitterExtensionFeatures;
+import cz.habarta.typescript.generator.emitter.TsModel;
+
+import java.util.stream.Collectors;
+
+public class EnumTypeAliasExtension extends EmitterExtension {
+    @Override
+    public EmitterExtensionFeatures getFeatures() {
+        final EmitterExtensionFeatures features = new EmitterExtensionFeatures();
+        return features;
+    }
+
+    @Override
+    public void emitElements(Writer writer, Settings settings, boolean exportKeyword, TsModel model) {
+        String enums = model.getTypeAliases().stream().map(e -> "\""+e.getName().getSimpleName()+"\"").collect(Collectors.joining(" | "));
+        writer.writeIndentedLine("type AllEnumClass = "+enums);
+    }
+}
